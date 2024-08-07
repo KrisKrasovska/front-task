688e478afc038eba3d2bb907505c3d80f08adcbe
Kristuwa <121851618+KrisKrasovska@users.noreply.github.com>
2024-08-07 12:36:51 +0300

    update form

--- js/sendForm.js
+++ js/sendForm.js
@@ -1,13 +1,90 @@
 const removePhoto = document.getElementById("remove-photo");
 const formRef = document.querySelector(".form");
 const backdrop = document.getElementById("backdrop");
-removePhoto.addEventListener("click", function () {
-  document.getElementById("organization-photo").value = "";
-});
+const choosePhoto = document.querySelector(".form-group__input-photo");
+const inputPhotoRef = document.getElementById("organization-photo");
+const phoneInput = document.getElementById("organization-phone");
 
+//наложение маски на форму
+document.addEventListener("DOMContentLoaded", function () {
+  const prefixNumber = (str) => {
+    if (str === "7") {
+      return "7 (";
+    }
+    if (str === "8") {
+      return "8 (";
+    }
+    if (str === "9") {
+      return "7 (9";
+    }
+    return "7 (";
+  };
+
+  // ======================================
+  phoneInput.addEventListener("input", (e) => {
+    const value = phoneInput.value.replace(/\D+/g, "");
+    const numberLength = 11;
+
+    let result;
+    if (phoneInput.value.includes("+8") || phoneInput.value[0] === "8") {
+      result = "";
+    } else {
+      result = "+";
+    }
+
+    //
+    for (let i = 0; i < value.length && i < numberLength; i++) {
+      switch (i) {
+        case 0:
+          result += prefixNumber(value[i]);
+          continue;
+        case 4:
+          result += ") ";
+          break;
+        case 7:
+          result += "-";
+          break;
+        case 9:
+          result += "-";
+          break;
+        default:
+          break;
+      }
+      result += value[i];
+    }
+    //
+    phoneInput.value = result;
+  });
+});
+// функция для валидации номера телефона
+const validatePhoneNumber = (phoneNumber) => {
+  const phoneRegex = /^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/;
+  return phoneRegex.test(phoneNumber);
+};
+// предпросмотр изображения
+inputPhotoRef.addEventListener("change", function (event) {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      choosePhoto.style = `background-image:url(${e.target.result});`;
+    };
+    reader.readAsDataURL(file);
+  }
+});
+// удаление фото при клике на крестик
+removePhoto.addEventListener("click", function () {
+  inputPhotoRef.value = "";
+  choosePhoto.style.backgroundImage = 'url("../images/photo.webp")';
+});
+// отправка формы
 formRef.addEventListener("submit", function (e) {
   e.preventDefault();
-
+  const phoneNumber = phoneInput.value;
+  if (!validatePhoneNumber(phoneNumber)) {
+    alert("Введите корректный номер телефона!");
+    return;
+  }
   if (e.target.checkValidity()) {
     const formData = new FormData(e.target);
     formData.forEach((value, name) => {
@@ -15,6 +92,8 @@ formRef.addEventListener("submit", function (e) {
     });
     alert("Форма отправлена!");
     formRef.reset();
+    inputPhotoRef.value = "";
+    choosePhoto.style.backgroundImage = 'url("../images/photo.webp")';
     backdrop.classList.add("is-hidden");
   } else {
     alert("Заполните все обязательные поля!");
